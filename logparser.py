512ccaf0bc545013f4501c54482dfd48b43c9e3f
V.Melentev <velikaya_tropa@mail.ru>
2020-01-20 00:22:44 +0300

    + file.close()

--- logparser.py
+++ logparser.py
@@ -23,6 +23,7 @@ file_list = glob.glob("*.tx")
 with open('logmessages.txt', 'w') as file:
 	input_lines = fileinput.input(file_list)
 	file.writelines(input_lines)
+	file.close()
 
 with open('logmessages.txt') as f:
 	for row in f:
