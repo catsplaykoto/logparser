dde86e926dfa363e422a964b8cf1764554a2486d
V.Melentev <velikaya_tropa@mail.ru>
2020-01-19 16:03:37 +0300

    first commit

---
+++ logparser.py
@@ -0,0 +1,17 @@
+import re
+import fileinput
+import glob
+
+file_list = glob.glob("*.tx")
+with open('logmessages.txt', 'w') as file:
+	input_lines = fileinput.input(file_list)
+	file.writelines(input_lines)
+
+with open('logmessages.txt') as f:
+	for row in f:
+		strings = ['alarm', 'Alarm']
+		for string in strings:
+			match = re.search(string, row)
+			if match:
+				print('Found "{}" in "{}"'.format(string, row))
+	f.close()
